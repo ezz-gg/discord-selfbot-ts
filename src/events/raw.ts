838b02d06012f0adb28b8c6ebfa749cb2693e5c8
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2022-07-16 07:46:37 +0900

    Add files via upload

--- src/events/raw.ts
+++ src/events/raw.ts
@@ -3,5 +3,5 @@ import * as Discord from "discord.js-selfbot-v13";
 import * as Logger from "../utils/logger";
 import { ClientManager } from "../bot";
 export default async (Client: Discord.Client, d) => {
-    ClientManager.updateVoiceState(d);
+    await ClientManager.updateVoiceState(d);
 }
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,7 +3,7 @@ import * as Discord from "discord.js-selfbot-v13";
 import * as Logger from "../utils/logger";
 import { ClientManager } from "../bot";
 export default async (Client: Discord.Client) => {
-	Logger.log(`Client is ready and is logged in as ${Client.user.tag}!`);
+	await Logger.log(`Client is ready and is logged in as ${Client.user.tag}!`);
 	Client.user.setStatus('online');
 	Client.user.setActivity(`Among Us`)
 	ClientManager.init(Client.user.id);
