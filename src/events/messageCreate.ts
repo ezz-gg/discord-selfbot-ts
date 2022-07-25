a879f31229c17b93d865eabb2f715b85535cabf4
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2022-07-26 08:45:51 +0900

    Update messageCreate.ts

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,12 +1,12 @@
 import * as Discord from "discord.js-selfbot-v13";
 import * as Logger from "../utils/logger";
-import { Ownerid, Prefix, Only_guild } from "../envs";
+import { Ownerid, Prefix } from "../envs";
 import * as fs from 'fs';
 import * as path from "path";
 import { player } from "../bot";
 import { RateLimiter } from 'discord.js-rate-limiter';
 
-const rateLimiter = new RateLimiter(1, 1500);
+const rateLimiter = new RateLimiter(1, 1000);
 
 export default async (Client: Discord.Client, message: Discord.Message) => {
 	
@@ -20,48 +20,46 @@ export default async (Client: Discord.Client, message: Discord.Message) => {
 	const skipaliases = /^(skip|sk|next|スキップ)$/i;
 
 
-	if (message.guild.id === Only_guild) {
-		if (message.content.startsWith(Prefix)) {
-			if (message.author.id === Ownerid || !JSON.parse(fs.readFileSync(path.join(__dirname,'../blacklist.json'), 'utf8')).hasOwnProperty(message.author.id)) {
-				let ratelimited = rateLimiter.take(message.author.id);
-				if (!ratelimited) {
-					switch (true) {
-						case (blacklistaliases.test(cmd)):
-							require(`../commands/blacklist`).run(Client, message, args);
-							Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
-							break
-						case (joinaliases.test(cmd)):
-							require(`../commands/join`).run(Client, message, args);
-							Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
-							break
-						case (leavealiases.test(cmd)):
-							require(`../commands/leave`).run(Client, message, args);
-							Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
-							break
-						case (skipaliases.test(cmd)):
-							require(`../commands/skip`).run(Client, message, args);
-							Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
-							break
-						default:
-							return
-					}
+	if (message.content.startsWith(Prefix)) {
+		if (message.author.id === Ownerid || JSON.parse(fs.readFileSync(path.join(__dirname,'../whitelist.json'), 'utf8')).hasOwnProperty(message.author.id)) {
+			let ratelimited = rateLimiter.take(message.author.id);
+			if (!ratelimited) {
+				switch (true) {
+					case (blacklistaliases.test(cmd)):
+						require(`../commands/blacklist`).run(Client, message, args);
+						Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
+						break
+					case (joinaliases.test(cmd)):
+						require(`../commands/join`).run(Client, message, args);
+						Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
+						break
+					case (leavealiases.test(cmd)):
+						require(`../commands/leave`).run(Client, message, args);
+						Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
+						break
+					case (skipaliases.test(cmd)):
+						require(`../commands/skip`).run(Client, message, args);
+						Logger.log(`CMDRUN：\n				Command:${cmd}\n				Guild:${message.guild.name}\n				Author:${message.author.tag}`);
+						break
+					default:
+						return
 				}
+			}
+		} else
+			return;
+	} else if (message.author.id !== Client.user.id && !message.author.bot && message.guild.channels.cache.some(channel => (channel.type === 'GUILD_VOICE' && channel.members.has(Client.user.id))) && !JSON.parse(fs.readFileSync(path.join(__dirname,'../blacklist.json'), 'utf8')).hasOwnProperty(message.author.id)) {
+		if (message.attachments.size) {
+			var message_content = message.cleanContent.replace(/(https?:\/\/[\x21-\x7e]+)/g, 'URL').replace(/\n| |　/g, '、') + "、添付ファイル";
+			if (message_content.length < 200) {
+				await player(message, "yomiage", message_content, message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
 			} else
-				return;
-		} else if (message.author.id !== Client.user.id && !message.author.bot && message.guild.channels.cache.some(channel => (channel.type === 'GUILD_VOICE' && channel.members.has(Client.user.id))) && !JSON.parse(fs.readFileSync(path.join(__dirname,'../blacklist.json'), 'utf8')).hasOwnProperty(message.author.id)) {
-			if (message.attachments.size) {
-				var message_content = message.cleanContent.replace(/(https?:\/\/[\x21-\x7e]+)/g, 'URL').replace(/\n| |　/g, '、') + "、添付ファイル";
-				if (message_content.length < 200) {
-					await player(message, "yomiage", message_content, message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
-				} else
-					await player(message, "yomiage", message_content.slice(196) + "以下省略", message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
-			} else if (!message.attachments.size) {
-				var message_content = message.cleanContent.replace(/(https?:\/\/[\x21-\x7e]+)/g, 'URL').replace(/\n| |　/g, '、');
-				if (message_content.length < 200) {
-					await player(message, "yomiage", message_content, message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
-				} else
-					await player(message, "yomiage", message_content.slice(196) + "以下省略", message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
-			};
+				await player(message, "yomiage", message_content.slice(196) + "以下省略", message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
+		} else if (!message.attachments.size) {
+			var message_content = message.cleanContent.replace(/(https?:\/\/[\x21-\x7e]+)/g, 'URL').replace(/\n| |　/g, '、');
+			if (message_content.length < 200) {
+				await player(message, "yomiage", message_content, message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
+			} else
+				await player(message, "yomiage", message_content.slice(196) + "以下省略", message.channel.id, message.member.voice.channel.id, message.author, message.guild.id, 100, 0);
 		};
 	};
-}
+};
