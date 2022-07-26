aec9c4e99e8b864a5b2c6d2f8923734bbf68a13b
NaeCqde <50701386+NaeCqde@users.noreply.github.com>
2022-07-26 17:12:37 +0900

    Update voiceStateUpdate.ts

--- src/events/voiceStateUpdate.ts
+++ src/events/voiceStateUpdate.ts
@@ -7,14 +7,16 @@ import * as path from "path";
 export default async (Client: Discord.Client, oldState, newState) => {
     if (oldState.guild.id === Only_guild && oldState.user.id !== Client.user.id && newState.user.id !== Client.user.id) {
         if (oldState.channelId === voice_channel || newState.channelId === voice_channel) {
-            if (oldState.channelId == null && newState.channelId != null) {
-                player(null, "vcmember_join", oldState.member.user.username, null, oldState.channelId, oldState.author, oldState.guild.id, 0, 0);
-            } else if (oldState.channelId != null && newState.channelId == null) {
-                player(null, "vcmember_leave", newState.member.user.username, null, newState.channelId, newState.author, newState.guild.id, 0, 0);
-            } else if (oldState.channelId != null && newState.channelId != null && newState.channelId == voice_channel) {
-                player(null, "vcmember_fromove", oldState.member.user.username, null, oldState.channelId, oldState.author, oldState.guild.id, 0, 0);
-            } else if (oldState.channelId != null && newState.channelId != null && newState.channelId != voice_channel) {
-                player(null, "vcmember_tomove", oldState.member.user.username, null, oldState.channelId, oldState.author, oldState.guild.id, 0, 0);
+            switch (true) {
+                case (oldState.channelId == null && newState.channelId != null):
+                    player(null, "vcmember_join", oldState.member.user.username, null, oldState.channelId, oldState.author, oldState.guild.id, 0, 0);
+                    break;
+                case (oldState.channelId != null && newState.channelId == null):
+                    player(null, "vcmember_leave", newState.member.user.username, null, newState.channelId, newState.author, newState.guild.id, 0, 0);
+                    break;
+                case (oldState.channelId != null && newState.channelId != null && newState.channelId != voice_channel):
+                    player(null, "vcmember_tomove", oldState.member.username, null, oldState.channelId, oldState.author, oldState.guild.id, 0, 0);
+                    break;
             };
         };
     };
